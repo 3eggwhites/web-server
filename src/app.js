4982a179e01b912678476267f6b33200cae05ed1
Ayan Pal <contact@ayanpal.co.in>
2020-08-27 01:13:10 +0530

    explored different send methods of express.js get

--- src/app.js
+++ src/app.js
@@ -3,19 +3,27 @@ const express = require('express');
 const app = express();
 
 app.get('', (req,res) => {
-    res.send('Hello express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req,res) => {
-    res.send('Help Page');
+    res.send([{
+        name: 'Ayan'
+    },
+    {
+        name: 'Ananya'
+    }]);
 })
 
 app.get('/about', (req,res) => {
-    res.send('About Page');
+    res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req,res) => {
-    res.send("Today's forecast");
+    res.send({
+        location: 'Howrah',
+        forecast: 'Rainy'
+    });
 });
 
 app.listen(4200, () => {
