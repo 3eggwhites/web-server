2a4433c344209519b1ea781ed33f6b17b9e38107
Ayan Pal <contact@ayanpal.co.in>
2020-08-30 21:35:35 +0530

    fixed a small issue

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,12 @@ const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = '';
+    messageTwo.textContent = '';
     messageOne.textContent = 'Loading...';
     const location = search.value;
     const queryUrl = '/weather?address='+location;
     fetch(queryUrl).then((response) => {
     response.json().then(({error, forecast, location}) => {
-        messageTwo.textContent = ''
         if (error) {
             return messageOne.textContent = error;
         }
