622a4f578d9f5c57569a1bbe00e312eded49ef19
Ayan Pal <contact@ayanpal.co.in>
2020-08-30 17:41:45 +0530

    added a form in ui to serach weather update by location

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
-
 fetch('http://localhost:4200/weather?address=!').then((response) => {
     response.json().then(({error, forecast, location}) => {
         if (error) {
@@ -12,4 +6,22 @@ fetch('http://localhost:4200/weather?address=!').then((response) => {
         console.log(forecast);
         console.log(location);
     });
+});
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    const location = search.value;
+    const queryUrl = 'http://localhost:4200/weather?address='+location;
+    fetch(queryUrl).then((response) => {
+    response.json().then(({error, forecast, location}) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(forecast);
+        console.log(location);
+    });
+});
+    e.preventDefault();
 });
--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.get('/help', (req,res) => {
 app.get('/weather', (req,res) => {
     if (!req.query.address) {
         return res.send({
-            error: 'Please provide an address'
+            error: 'Please provide a location'
         });
     }
     const address = req.query.address;
