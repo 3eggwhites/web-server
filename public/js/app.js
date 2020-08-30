ae6b97db9835c41a7bbb3c105ca39dd5efa1083b
Ayan Pal <contact@ayanpal.co.in>
2020-08-30 17:19:01 +0530

    used fetch api in client side app.js file to call the weather forecast api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-console.log('clientside js');
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    });
+});
+
+fetch('http://localhost:4200/weather?address=!').then((response) => {
+    response.json().then(({error, forecast, location}) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(forecast);
+        console.log(location);
+    });
+});
