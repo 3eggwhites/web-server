2bc6196878e3210d0afcd9591bbd0fabc755560a
Ayan Pal <contact@ayanpal.co.in>
2020-08-30 18:03:59 +0530

    displayed weather data on ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,21 @@
-fetch('http://localhost:4200/weather?address=!').then((response) => {
-    response.json().then(({error, forecast, location}) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log(forecast);
-        console.log(location);
-    });
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
+    messageOne.textContent = '';
+    messageOne.textContent = 'Loading...';
     const location = search.value;
     const queryUrl = 'http://localhost:4200/weather?address='+location;
     fetch(queryUrl).then((response) => {
     response.json().then(({error, forecast, location}) => {
+        messageTwo.textContent = ''
         if (error) {
-            return console.log(error);
+            return messageOne.textContent = error;
         }
-        console.log(forecast);
-        console.log(location);
+        messageOne.textContent = forecast;
+        messageTwo.textContent = location;
     });
 });
     e.preventDefault();
