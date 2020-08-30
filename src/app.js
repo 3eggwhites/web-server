60645d7f1c32210d96cc314aab0d722b763d0c98
Ayan Pal <contact@ayanpal.co.in>
2020-08-30 21:22:13 +0530

    added text on about and help page also changed about page title

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.get('', (req,res) => {
 
 app.get('/about', (req,res) => {
     res.render('about', {
-        title: 'About Me',
+        title: 'About',
         name: 'Ayan Pal'
     })
 });
@@ -36,7 +36,7 @@ app.get('/about', (req,res) => {
 app.get('/help', (req,res) => {
     res.render('help', {
         title: 'Help',
-        helpMessage: 'Help yourself',
+        helpMessage: 'To search weather update for a location, enter location name or address in the serachbox and hit the search button',
         name: 'Ayan Pal'
     })
 });
